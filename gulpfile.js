c5db2fef4838f1bf1ba4c4536d411e7666d7a0f8
Ajay Sabhaney <ajay@mothercreative.com>
2014-02-13 02:59:45 -0700

    Some more cleanup. Precompilation of templates.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,14 @@
 
-var gulp = require('gulp'),
-    less = require('gulp-less'),
-    path = require('path');
+var gulp    = require('gulp'),
+    clean   = require('gulp-clean'),
+    concat  = require('gulp-concat'),
+    exec    = require('gulp-exec'),
+    ext     = require('gulp-ext-replace'),
+    less    = require('gulp-less'),
+    swig    = require('./tmp/gulp-swig-precompiler'),
+    uglify  = require('gulp-uglify'),
+    wrap    = require('gulp-wrap'),
+    path    = require('path');
 
 //---------------------------------------------------------
 // Constants
@@ -14,19 +21,34 @@ var BUILD_DIR = './build';
 //---------------------------------------------------------
 
 gulp.task('clean', function() {
-  return gulp.src('public', { read: false })
-    .pipe(clean());
+   return gulp.src(BUILD_DIR, { read: false })
+      .pipe(clean());
 });
 
 //---------------------------------------------------------
-// LESS Task
+// Less Compilation Task
 //---------------------------------------------------------
 
 gulp.task('less', function() {
-  var options = { paths: [ path.join(__dirname, 'lib', 'less') ] };
-  return gulp.src('lib/less/**/*.less')
-    .pipe(less(options))
-    .pipe(gulp.dest(BUILD_DIR + '/css'));
+   var options = { paths: [ path.join(__dirname, 'lib', 'less') ] };
+   return gulp.src('lib/less/**/*.less')
+      .pipe(less(options))
+      .pipe(gulp.dest(BUILD_DIR + '/css'));
+});
+
+//---------------------------------------------------------
+// Templates
+//---------------------------------------------------------
+   
+gulp.task('templates', function() {
+   var base = path.join(__dirname, 'lib/views');
+   return gulp.src('lib/views/partials/**/*.html', { base : base })
+      .pipe(swig({ filters : require('./lib/js/swig.helpers') }))
+      .pipe(ext('.html'))
+      .pipe(wrap('carbon.templates.register("<%= file.relative %>", <%= contents %>);'))
+      .pipe(concat('templates.js'))
+      .pipe(uglify())
+      .pipe(gulp.dest(BUILD_DIR + '/js'));
 });
 
 //------------------------------------------------
@@ -35,11 +57,12 @@ gulp.task('less', function() {
 
 gulp.task('watch-assets', function () {
    gulp.watch('lib/less/**/*.less', ['less']);
+   gulp.watch('lib/views/partials/**/*.html', ['templates']);
 });
 
 //------------------------------------------------
 // Main Entrypoints
 //------------------------------------------------
 
-gulp.task('default', ['less', 'watch-assets']);
-gulp.task('build', ['less']);
+gulp.task('build', ['less', 'templates']);
+gulp.task('default', ['build', 'watch-assets']);
